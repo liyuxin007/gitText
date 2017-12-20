a3db7875e3b9d6c5ee97c8fed4c83b971c731002
liyuxin007 <1986636619@qq.com>
2017-12-20 11:30:23 +0800

    python

---
+++ pygame2/ship.py
@@ -0,0 +1,19 @@
+import pygame
+class Ship():
+    def __init__(self, screen):
+        self.screen = screen
+        self.image = pygame.image.load('../images/2.jpg')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+    #     移动标志
+        self.moving_right = False
+        self.moving_left = False
+    def update(self):
+        if self.moving_right:
+            self.rect.centerx +=1
+        if self.moving_left:
+            self.rect.centerx -=1
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
